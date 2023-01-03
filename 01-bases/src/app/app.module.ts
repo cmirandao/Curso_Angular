d9dcccb6ed4fb212c381c2386aa93de38388514d
Carolina <miranda.olivares@gmail.com>
2023-01-03 19:20:04 -0300

    ejercicios udemy

---
+++ 01-bases/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+//Crear un modulo llamado ContadorModule, declaraciones y exportaciones
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
